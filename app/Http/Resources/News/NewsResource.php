7a3f0d9a1cdb479e0a4ff548d449823550b87872
Vladimir <v0v.voron2005@yandex.ru>
2023-06-19 00:29:25 +0300

    Add NewsResource.php

---
+++ app/Http/Resources/News/NewsResource.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Resources\News;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class NewsResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'title' => $this->title,
+            'description' => $this->description,
+            'created_at' => $this->created_at,
+        ];
+    }
+}
