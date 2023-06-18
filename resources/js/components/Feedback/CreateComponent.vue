18afec95e2dae20563c6e5debb261da7d4220851
Vladimir <v0v.voron2005@yandex.ru>
2023-06-18 20:39:24 +0300

    Add create feedback component

---
+++ resources/js/components/Feedback/CreateComponent.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="container">
+        <h1 class="feedback_title">Форма обратной связи</h1>
+        <form id="feedback-form">
+            <div class="form-group">
+                <label for="name">*ФИО:</label>
+                <input class="error" type="text" id="name" name="name" required>
+                <span class="error-message">Пожалуйста, введите ФИО.</span>
+            </div>
+            <div class="form-group">
+                <label for="address">Адрес:</label>
+                <input type="text" id="address" name="address">
+            </div>
+            <div class="form-group">
+                <label for="phone">*Телефон:</label>
+                <input class="error" type="tel" id="phone" name="phone" required>
+                <span class="error-message">Пожалуйста, введите корректный телефон.</span>
+            </div>
+            <div class="form-group">
+                <label for="email">*E-mail:</label>
+                <input class="error" type="email" id="email" name="email" required>
+                <span class="error-message">Пожалуйста, введите корректный e-mail.</span>
+            </div>
+            <button type="submit">Отправить</button>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'FeedbackComponent',
+}
+</script>
+
+<style scoped>
+
+</style>
