a237584f9d42b87cd5c3b3a011c168c5de952b6e
Vladimir <v0v.voron2005@yandex.ru>
2023-06-18 16:21:01 +0300

    Init project

---
+++ resources/js/app.js
@@ -0,0 +1,11 @@
+import './bootstrap';
+
+import { createApp } from 'vue';
+import Example from './components/Example.vue'
+const app = createApp({
+    components: {
+        Example,
+    }
+})
+
+app.mount(`#app`);
---
+++ resources/js/components/Example.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>Ex 123</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Example',
+    }
+</script>
+
+<style scoped>
+
+</style>
