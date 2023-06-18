96e2cdefa535ba03a3e55991909abfb9cb0d58f8
Vladimir <v0v.voron2005@yandex.ru>
2023-06-19 00:29:00 +0300

    Add IndexController.php for news

---
+++ app/Http/Controllers/News/IndexController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\News;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\News\NewsResource;
+use App\Models\News;
+
+class IndexController extends Controller
+{
+    public function __invoke()
+    {
+        return NewsResource::collection(News::all());
+    }
+}
