8550a8a5a69948b9482857e0222ef9e5ab828f90
Vladimir <v0v.voron2005@yandex.ru>
2023-06-18 19:32:36 +0300

    Add vue components for main pages

--- resources/js/components/FeedbackComponent.vue
+++ resources/js/components/FeedbackComponent.vue
@@ -1,13 +1,13 @@
 <template>
     <div>
-        <h1>Ex 123</h1>
+        <h1>Feedback</h1>
     </div>
 </template>
 
 <script>
-    export default {
-        name: 'Example',
-    }
+export default {
+    name: 'FeedbackComponent',
+}
 </script>
 
 <style scoped>
