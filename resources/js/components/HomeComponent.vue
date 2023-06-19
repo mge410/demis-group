fd697253196b2eb4360e19087bd915ed402c228b
Vladimir <v0v.voron2005@yandex.ru>
2023-06-19 08:42:14 +0300

    Update HomeComponent.vue

--- resources/js/components/HomeComponent.vue
+++ resources/js/components/HomeComponent.vue
@@ -1,15 +1,52 @@
 <template>
-    <div>
-        <h1>Home</h1>
+    <div class="news-container news-block">
+        <template v-for="item in news">
+            <div class="news-tile">
+                <h2 class="news-title">{{ item.title }}</h2>
+                <p class="news-text">{{ item.description }}</p>
+                <p class="news-date">Дата публикации: {{ item.created_at }}</p>
+            </div>
+        </template>
     </div>
+    <nav>
+        <ul>
+            <li>
+                <router-link :to="{ name: 'news.index'  }">Все новости</router-link>
+            </li>
+            <li>
+                <router-link :to="{ name: 'feedback.index'  }">Форма обратной связи</router-link>
+            </li>
+        </ul>
+    </nav>
 </template>
 
 <script>
 export default {
     name: 'HomeComponent',
+
+    data(){
+        return {
+            news: null,
+        }
+    },
+
+    mounted() {
+        this.getNews()
+    },
+
+    methods: {
+        getNews(){
+            axios.get('/api/home/news/')
+                .then(response => {
+                    this.news = response.data.data
+                })
+        }
+    }
 }
 </script>
 
 <style scoped>
-
+    li{
+        margin-bottom: 15px;
+    }
 </style>
